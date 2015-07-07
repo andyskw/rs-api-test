c6ef7f67a18d5a3b2c8040dbe4f7984492a2fe65
András Iványi <andras.ivanyi@gmail.com>
2015-07-07 16:21:04 +0200

    Strict mode enabled, and added TODO-s to index.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 
 var express = require('express');
 var http = require("http");
@@ -8,12 +9,17 @@ var users = require("./routes/users");
 
 
 //Setting routes
+//TODO: router initialization could be moved out to an external module
 var router = require("express").Router();
 router.post("/registration", registration.postRegistration);
 router.get("/users", users.getUsers);
 
+
 //Initial Express setup
+//TODO: configuration handling
 app.set('port', process.env.PORT ? process.env.PORT : 3000);
+
+//TODO: CORS support?
 app.options("*", router);
 app.get("*", router);
 app.put("*", router);
@@ -21,5 +27,6 @@ app.post("*", router);
 
 //Let's get to work. :D
 http.createServer(app).listen(app.get('port'), function() {
+       //TODO: Logging?
        console.info("RS API test started on port " + app.get('port'));
 });
