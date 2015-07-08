4c6b670653a75c38580159f107e00c348dd1a6cf
András Iványi <andras.ivanyi@gmail.com>
2015-07-08 10:38:12 +0200

    Added initial implementation for GET /users endpoint.

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,27 @@
+var db = require("../db");
+var Q = require("q");
 exports.getUsers = function(req,res) {
+    db.UserToken.find( {
+        tokenValue : req.params.token
+    }).count().execQ()
+        .then(function (data) {
+            var deferred = Q.defer();
+            if (data === 0) {
+                deferred.reject("INVALID_TOKEN");
+            } else {
+                deferred.resolve();
+            }
+            return deferred.promise;
+        })
+        .then(function (f) {
+            return db.User.findQ();
+        })
+        .then(function(users) {
+            //TODO: attributes(name), type for each user...
+            res.send(JSON.stringify({data: users}));
+        })
+        .fail(function (reason) {
+            res.send({error: reason});
+        }).done();
   res.send(JSON.stringify({world: "Hello"}));
 }
